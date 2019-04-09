1ce0871ec8742512db98f483f5cc9cb3aa35d144
zacharyhaag <32313367+zacharyhaag@users.noreply.github.com>
2019-04-09 10:35:23 -0700

    Create person.cpp

---
+++ person.cpp
@@ -0,0 +1,28 @@
+#include "person.h"
+
+Person::Person(int cardNo, bool act, string fName, string lName) {
+    // complete constructor
+}
+
+string Person::getFirstName() {
+    return ""; // complete
+}
+
+string Person::getLastName() {
+    return ""; // complete
+}
+
+int Person::getId() {
+    return 0; // complete
+}
+
+void Person::setActive(bool act) {
+} // complete
+
+bool Person::isActive() {
+    return true; // complete
+}
+
+string Person::fullName() {
+    return ""; // complete
+}
