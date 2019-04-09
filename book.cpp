c0b9559161e3be983ee22b41ab80344c4e03da62
zacharyhaag <32313367+zacharyhaag@users.noreply.github.com>
2019-04-09 10:33:58 -0700

    Create book.cpp

---
+++ book.cpp
@@ -0,0 +1,27 @@
+#include "book.h"
+
+Book::Book(int id, string bookName, string auth, string cat) {
+    // complete constructor
+}
+
+string Book::getTitle() {
+    return ""; // complete
+}
+
+string Book::getAuthor() {
+    return ""; // complete
+}
+
+string Book::getCategory() {
+    return ""; // complete
+}
+
+int Book::getId() {
+    return 0; // complete
+}
+void Book::setPersonPtr(Person * ptr) {
+} // complete
+
+Person * Book::getPersonPtr() {
+    return nullptr; // complete
+}
